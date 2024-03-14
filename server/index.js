72caafe8d65d0eeb925c9354d8be91193266bbb6
theziko1 <zakariaghachim96@gmail.com>
2024-03-14 10:35:37 +0000

    Environment-Setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
-console.log("first")
+import { config } from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+
+config()
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(()=>{
+   console.log("Connected to the DATABASE !")
+})
+.catch((err)=>{
+    console.log(`No Connexion ${err} !`)
+})
+
+const PORT = process.env.PORT
+const app = express()
+
+
+app.listen(PORT,()=>{
+    console.log(`🚀 ~ Server running  ~ : ${PORT}` ) 
+})
