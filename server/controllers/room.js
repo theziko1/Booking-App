fe755fed860180f7a643f2d73b8c69e73667e28c
theziko1 <zakariaghachim96@gmail.com>
2024-03-16 16:06:56 +0000

    create crud operations

---
+++ server/controllers/room.js
@@ -0,0 +1,57 @@
+import Room from "../models/Room"
+
+
+// Create a Room
+export const AddRoom = async (req,res) => {
+    const newRoom = new Room(req.body)
+    try {
+        const savedRoom = await newRoom.save()
+        res.status(201).json(savedRoom)
+    } catch (error) {
+        res.status(500).json(error)
+    } 
+}
+
+// Update a Room
+export const UpdateRoom = async (req,res) => {
+   
+    try {
+        const UpdatedRoom = await Room.findByIdAndUpdate(req.params.id,{$set : req.body},{new:true})
+        res.status(201).json(UpdatedRoom)
+    } catch (error) {
+        res.status(500).json(error)
+    } 
+}
+
+// Delete a Room
+export const DeleteRoom = async (req,res) => {
+   
+    try {
+        await Room.findByIdAndDelete(req.params.id)
+        res.status(201).json("Room has been deleted")
+    } catch (error) {
+        res.status(500).json(error)
+    } 
+}
+
+// Get a Room
+export const GetOneRoom = async (req,res) => {
+   
+    try {
+        const room = await Room.findById(req.params.id)
+        res.status(201).json(room)
+    } catch (error) {
+        res.status(500).json(error)
+    } 
+}
+
+// Get hotels
+export const GetAllRooms = async (req,res) => {
+   
+    try {
+        const rooms =await Room.find()
+        res.status(201).json(rooms)
+    } catch (error) {
+        res.status(500).json(error)
+    } 
+}
