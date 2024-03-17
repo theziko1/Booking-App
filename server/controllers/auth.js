47c4ccede7d6d367b47c9153099b520aeda47c4a
theziko1 <zakariaghachim96@gmail.com>
2024-03-17 15:09:21 +0000

    create login method

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -18,3 +18,22 @@ export const register = async(req,res,next) => {
   }
 }
 
+export const login = async(req,res,next) => {
+   try {
+     const user = await User.findOne({username : req.body.username})
+     if (!user) return next(createError(404,"User not found !"))
+
+     const isPasswordCorrect = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+      if (!isPasswordCorrect)
+      return next(createError(400, "Wrong password or username!"));
+
+      const { password, isAdmin, ...otherDetails } = user._doc;
+
+      res.status(200).send({...otherDetails})
+   } catch (error) {
+      next(error)
+   }
+}
